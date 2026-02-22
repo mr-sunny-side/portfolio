2dbc167ed4342a2d79f9b4c4eb7bc22d07c527c1
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-02-22 21:10:46 +0900

    02-22

--- ex31/01_ex31.py
+++ ex31/01_ex31.py
@@ -1,5 +1,5 @@
 """
-	02-21: 新しい01への取り組み開始
+	02-21: Enumクラスの理解から(ex自体は完了)
 
 """
 from fastapi import FastAPI
@@ -15,5 +15,14 @@ async def	handle_ping():
 	return {"status": "ok"}
 
 @app.get("/echo/{text}")
-async def	handle_echo(text):
+async def	handle_echo(text: str):
 	return {"echo": text}
+
+# /items/idではエラーになるので、先に宣言されている必要がある
+@app.get("/items/me")
+async def	handle_me():
+	return {"item_id": "me"}
+
+@app.get('/items/{id}')
+async def	handle_items(id: int):
+	return {"item_id": id}
