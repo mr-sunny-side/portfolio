e480695cabdffd68802d5c47b0b93ff63f84fd42
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-02-22 20:30:27 +0900

    02-22

--- ex31/01_ex31.py
+++ ex31/01_ex31.py
@@ -2,3 +2,18 @@
 	02-21: 事前解説を読むところから
 
 """
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def	handle_index():
+	return {"message": "Hello FastAPI!"}
+
+@app.get("/ping")
+async def	handle_ping():
+	return {"status": "ok"}
+
+@app.get("/echo/{text}")
+async def	handle_echo(text):
+	return {"echo": text}
