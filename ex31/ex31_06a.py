91c0471c28868723006e255e459d265eff864f9b
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-02-27 21:14:27 +0900

    02-27

--- ex31/ex31_06a.py
+++ ex31/ex31_06a.py
@@ -3,3 +3,16 @@
 			- PUT, DELETEの追加
 
 """
+from typing import Annotated
+from fastapi import FastAPI, Path, Query
+from sqlmodel import SQLModel, Field, create_engine,
+
+app = FastAPI()
+
+@app.get("/")
+async def handle_index():
+	return {"message": "Hello FastAPI !"}
+
+@app.get("/echo/{text}")
+async def handle_echo(text: str):
+	return {"echo": text}
