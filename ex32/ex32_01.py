b59968aaf61a6982d413dc7dca33b93c7ce4d8c1
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-03-01 21:30:59 +0900

    03-01

--- ex32/ex32_01.py
+++ ex32/ex32_01.py
@@ -1,12 +1,12 @@
 from typing import Annotated
 from fastapi import FastAPI, Depends
-from fastapi.security import OAuth2AuthorizationCodeBearer
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 
 # リクエストヘッダーからbearerトークンを抽出するオブジェクト
 # tokenUrlは認証を行うエンドポイントを指定
-oauth2 = OAuth2AuthorizationCodeBearer(tokenUrl="token")
+oauth2 = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.get("/items")
 async def handle_items(token: Annotated[str, Depends(oauth2)]):
