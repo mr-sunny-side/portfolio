d212ea655ff04ec33b98b752b23529b02ed4b7be
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-03-18 19:00:34 +0900

    03-18

---
+++ ex34/ex34_01/main.py
@@ -0,0 +1,153 @@
+"""
+	03-18:	アプリの構築
+			- item認証エンドポイントの作成から
+			- user/register, users/me=delete, users/me/items
+			- items=post, items=delete
+			マイグレーションの作成
+			dockerイメージの作成
+
+"""
+from fastapi import FastAPI, Depends, HTTPException, Response
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from typing import Annotated
+from sqlmodel import Session, create_engine, select
+from pwdlib import PasswordHash
+from datetime import datetime, timedelta, timezone
+import jwt
+from jwt.exceptions import InvalidTokenError
+from dotenv import load_dotenv
+import os
+
+from models import Token, User, UserResponse, UserEx01, ItemEx01
+
+load_dotenv()
+dummy_hasher = PasswordHash.recommended()
+app = FastAPI()
+engine = create_engine("postgresql://ex33:secret@localhost/ex33_db")
+oauth2 = OAuth2PasswordBearer(tokenUrl="token")
+
+DUMMY = dummy_hasher.hash("dummy")
+KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+def get_session():
+	with Session(engine) as session:
+		yield session
+
+def auth_user(
+	username: str,
+	password: str,
+	session: Session
+) -> UserEx01 | None:
+	hasher = PasswordHash.recommended()
+
+	statement = select(UserEx01).where(UserEx01.username == username)
+	db_user = session.exec(statement).first()
+	if not db_user:
+		hasher.verify(password, DUMMY)	# 疑似検証
+		return None
+
+	if not hasher.verify(password, db_user.password):
+		return None
+	return db_user
+
+def create_token(
+	user_sub: dict,
+	token_expire: timedelta | None = None
+) -> str:
+	copy_sub = user_sub.copy()
+
+	if token_expire:
+		expire = datetime.now(timezone.utc) + token_expire
+	else:
+		expire =datetime.now(timezone.utc) + timedelta(minutes=30)
+
+	copy_sub.update({"exp": expire})
+	token = jwt.encode(copy_sub, KEY, ALGORITHM)
+	return token
+
+def get_cur_user(
+	token: Annotated[str, Depends(oauth2)],
+	session: Annotated[Session, Depends(get_session)]
+) -> UserEx01:
+	error_detail = HTTPException(
+		status_code=401,
+		detail="Authentication failed",
+		headers={"WWW-Authenticate": "Bearer"}
+	)
+
+	try:
+		payload = jwt.decode(token, KEY, [ALGORITHM])
+		username = payload.get("sub")
+		if not username:
+			raise error_detail
+	except InvalidTokenError:
+		raise error_detail
+
+	statement = select(UserEx01).where(UserEx01.username == username)
+	db_user = session.exec(statement).first()
+	if not db_user:
+		raise error_detail
+	return db_user
+
+
+@app.post("/users/register")
+async def handle_users_register(
+	user: User,
+	session: Annotated[Session, Depends(get_session)]
+) -> UserResponse:
+	hasher = PasswordHash.recommended()
+
+	db_user = UserEx01.model_validate(user)
+	db_user.password = hasher.hash(db_user.password)	# パスをハッシュ化して保存
+
+	session.add(db_user)
+	session.commit()
+	session.refresh(db_user)
+	return db_user
+
+@app.delete("/users/me", status_code=204)
+async def handle_delete_users(
+	cur_user: Annotated[UserEx01, Depends(get_cur_user)],
+	session: Annotated[Session, Depends(get_session)]
+):
+	session.delete(cur_user)
+	session.commit()
+	return Response(status_code=204)
+
+@app.post("/token")
+async def handle_token(
+	form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+	session: Annotated[Session, Depends(get_session)]
+) -> Token:
+	# ユーザー認証、データの取り出し
+	db_user = auth_user(
+		form_data.username,
+		form_data.password,
+		session
+	)
+
+	if not db_user:
+		raise HTTPException(
+			status_code=401,
+			detail="Authentication failed",
+			headers={"WWW-Authenticate": "Bearer"}
+		)
+
+	# トークン作成
+	token_expire = timedelta(minutes=30)
+	token = create_token(
+		user_sub={"sub": db_user.username},
+		token_expire=token_expire
+	)
+
+	return Token(
+		access_token=token,
+		token_type="Bearer"
+	)
+
+@app.post("/items")
+async def handle_add_items(
+
+):
+	pass
---
+++ ex34/ex34_01/models.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
+
+class Token(BaseModel):
+	access_token: str
+	token_type: str
+
+class User(BaseModel):
+	username: str
+	password: str
+	email: str | None = None
+
+class UserResponse(BaseModel):
+	id: int
+	username: str
+	email: str | None = None
+
+class UserEx01(SQLModel, table=True):
+	id: int = Field(default=None, primary_key=True)
+	username: str
+	password: str
+	email: str | None = None
+	disabled: bool = False
+	items: list["ItemEx01"] = Relationship(back_populates="owner")	# Itemテーブルとの関連付け、参照ができる
+
+class ItemEx01(SQLModel, table=True):
+	id: int = Field(default=None, primary_key=True)
+	name: str
+	price: int
+	user_id: int | None = Field(default=None, foreign_key="userex01.id")	# Userテーブルのidを関連付けて付与、小文字でかく
+	owner: "UserEx01" = Relationship(back_populates="items")
