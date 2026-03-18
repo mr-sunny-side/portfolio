5b695595275b8b3a411e09be0df1ecfeb44c9020
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-03-18 16:59:19 +0900

    03-18

---
+++ ex33/ex33_07/models.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
+
+
+# 受取用Item型
+class Item(BaseModel):
+	name: str
+	price: int = Field(ge=0)
+
+# レスポンス用Item型
+class ItemResponse(SQLModel):
+	id: int
+	name: str
+	price: int = Field(ge=0)
+
+# ユーザーから受取用Userデータ型
+class User(BaseModel):
+	username: str
+	password: str
+	email: str | None = None
+
+# レスポンス用Userデータ型
+class UserResponse(BaseModel):	# フィルタするために新しく定義
+	id: int
+	username: str
+	email: str | None = None
+	disabled: bool | None = False
+	items: list[ItemResponse] | None = None
+
+class Token(BaseModel):
+	access_token: str
+	token_type: str
+
+class UserEx07(SQLModel, table=True):
+	id: int | None = Field(default=None, primary_key=True)
+	username: str
+	password: str
+	email: str | None = None
+	disabled: bool | None = False
+	items: list["ItemEx07"] = Relationship(back_populates="owner")
+
+class ItemEx07(SQLModel, table=True):
+	id: int | None = Field(default=None, primary_key=True)
+	name: str
+	price: int
+	user_id: int | None = Field(default=None, foreign_key="userex07.id") # foreign_keyは全部小文字
+	owner: "UserEx07" = Relationship(back_populates="items")		# User型でのItem型の定義
