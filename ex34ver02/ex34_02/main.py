7924bf949a1de7852ec6c27fb845ad15ea14ed45
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-04-08 19:40:36 +0900

    04-08

---
+++ ex34ver02/ex34_02/main.py
@@ -0,0 +1,17 @@
+"""
+	04-08: プロジェクトdirの環境構築から
+	- 依存関係ファイルの作成
+	- 新規DBの作成(postgresql)
+	- alembicのインストール(コンテナの中でやったほうがいい？)
+	- .envファイルの作成
+
+	03-22:	アプリの構築
+			- token											 -
+			- user/register, users/me=delete, users/me/items -
+			- items=post, items=delete						 -
+			- dockerイメージの作成							 -
+			- engineの記述(db:5433へ接続)					 -
+			- マイグレーションの作成(コンテナ内で実行)		 -
+
+
+"""
