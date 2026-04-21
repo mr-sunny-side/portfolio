5f7f0490e547f1cf2dd6efe35513cd04640643a9
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-04-21 21:22:43 +0900

    04-21

--- ex34ver02/remaind02/main.py
+++ ex34ver02/remaind02/main.py
@@ -6,13 +6,13 @@
 	 - .envファイルの作成										- 完了
 
 	### アプリの構築
-	 - token											 - 完了
-	 - user/register									 - 完了
-	 - users=get										 -
-	 - users=delete										 -
-	 - items=post										 -
-	 - items=delete										 -
-	 - コードのレビュー									 -
+	 - token			 - 完了
+	 - user/register	 - 完了
+	 - users=get		 - 完了
+	 - users=delete		 - 完了
+	 - items=post		 - 完了
+	 - items=delete		 -
+	 - コードのレビュー	 -
 
 	 - dockerイメージの作成								 -
 	 - engineの記述(db:5433へ接続)						 -
@@ -20,8 +20,8 @@
 
 
 """
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import FastAPI, Depends, HTTPException, Response, Path
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from sqlmodel import Session, create_engine, select
 from typing import Annotated
 from dotenv import load_dotenv
@@ -29,6 +29,7 @@ import os
 from pwdlib import PasswordHash
 from datetime import datetime, timezone, timedelta
 import jwt
+from jwt.exceptions import InvalidTokenError
 
 from models import User, Item, UserResponse, ItemResponse, UserDB, ItemDB, Token
 
@@ -40,6 +41,7 @@ ALGORITHM = os.environ("ALGORITHM")
 
 app = FastAPI()
 engine = create_engine(DATABASE_URL)
+oauth2 = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_session():
 	with Session(engine) as session:
@@ -80,6 +82,33 @@ def create_token(
 	token = jwt.encode(copy_sub, KEY, ALGORITHM)
 	return token
 
+def get_cur_users(
+	token: Annotated[str, Depends(oauth2)],
+	session: Annotated[Session, Depends(get_session)]
+) -> UserDB:
+	error_detail = HTTPException(
+		status_code=401,
+		detail="Authentication failed",
+		headers={"WWW-Authenticate": "Bearer"}
+	)
+
+	# トークンのデコード、ユーザー名の取り出し
+	try:
+		payload = jwt.decode(token, KEY, [ALGORITHM])
+		username = payload.get("sub")
+		if not username:
+			raise error_detail
+	except InvalidTokenError:
+		raise error_detail
+
+	# ユーザーDBの取り出し
+	statement = select(UserDB).where(UserDB.username == username)
+	db_user = session.exec(statement).first()
+	if not db_user:
+		raise error_detail
+	return db_user
+
+# トークンの作成
 @app.post("/token")
 async def handle_token(
 	form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
@@ -111,7 +140,7 @@ async def handle_token(
 		token_type="Bearer"
 	)
 
-
+# ユーザーの追加
 @app.post("/users/register")
 async def handle_add_users(
 	user: User,
@@ -127,3 +156,73 @@ async def handle_add_users(
 	session.commit()
 	session.refresh(db_user)
 	return db_user
+
+# ユーザーの参照(トークン必須)
+@app.get("/users")
+async def handle_all_users(
+	cur_user: Annotated[UserDB, Depends(get_cur_users)],
+	session: Annotated[Session, Depends(get_session)]
+) -> list[UserResponse]:
+
+	# 全ユーザーの取得
+	db_users = session.exec(select(UserDB)).all()
+	return db_users
+
+# 自分のユーザーデータの削除
+@app.delete("/users", status_code=204)
+async def handle_delete_users(
+	cur_user: Annotated[UserDB, Depends(get_cur_users)],
+	session: Annotated[Session, Depends(get_session)]
+):
+	# ユーザーアイテムの削除
+	for item in cur_user.items:
+		session.delete(item)
+	session.delete(cur_user)
+	session.commit()
+	return Response(status_code=204)
+
+# アイテムの追加(トークン必須)
+@app.post("/items")
+async def handle_add_items(
+	item: Item,
+	cur_user: Annotated[UserDB, Depends(get_cur_users)],
+	session: Annotated[Session, Depends(get_session)]
+) -> ItemResponse:
+
+	# アイテムDB型へ変更, ユーザーIDの付与
+	db_item = ItemDB.model_validate(item)
+	db_item.user_id = cur_user.id
+
+	# DBへ保存
+	session.add(db_item)
+	session.commit()
+	session.refresh(db_item)
+	return db_item
+
+# キーを指定してアイテムを削除(自分のアイテムのみ可)
+@app.delete("/items/{id}", status_code=204)
+async def handle_delete_items(
+	id: Annotated[int, Path(ge=1)],
+	cur_user: Annotated[UserDB, Depends(get_cur_users)],
+	session: Annotated[Session, Depends(get_session)]
+):
+
+	# アイテムの取得
+	db_item = session.get(ItemDB, id)
+	if not db_item:
+		raise HTTPException(
+			status_code=404,
+			detail="Item not found"
+		)
+
+	# 自分のアイテムか確認
+	if not db_item.user_id == cur_user.id:
+		raise HTTPException(
+			status_code=403,
+			detail="Not authorized"
+		)
+
+	# アイテムの削除
+	session.delete(db_item)
+	session.commit()
+	return Response(status_code=204)
